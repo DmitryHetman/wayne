2bbd9aa21f0c287b5fc5fddccc3ea7a3bb080508
Lukas Wagner <lukaswagner93@googlemail.com>
2014-01-17 08:50:11 +0100

    Create wayne-launch.hpp

---
+++ src/wayne-launch.hpp
@@ -0,0 +1,19 @@
+/* wayne-launch.hpp
+*
+* Author: Lukas Wagner
+* Date of creation: Jan 17 2014
+*
+*/
+
+#ifndef __WAYNE_LAUNCH_HPP__
+#define __WAYNE_LAUNCH_HPP__
+
+
+class WayneLaunch {
+public:
+        
+private:
+        
+}
+
+#endif
